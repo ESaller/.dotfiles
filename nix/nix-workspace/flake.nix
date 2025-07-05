89fd8d8ddde88802b76cfdae3b0f8f3a63ef1786
Eduard Saller <ESaller@users.noreply.github.com>
2025-07-05 11:21:22 +0200

    Update flake.nix to include separate linux fallback

--- nix/nix-workspace/flake.nix
+++ nix/nix-workspace/flake.nix
@@ -1,4 +1,3 @@
-# flake.nix skeleton:
 {
   # The 'inputs' section of a flake.nix file specifies external dependencies or resources
   # that our flake will rely on. Here we define a single input.
@@ -11,106 +10,136 @@
 
   # The 'outputs' function is where the bulk of a flake's functionality is defined.
   # It takes the inputs defined above and produces a set of derivations.
-  outputs =
-    { self, nixpkgs }:
-    {
+  outputs = { self, nixpkgs }:
+    let
+      # Here, we list all the systems we want to support.
+      # 'aarch64-darwin' is for Apple Silicon Macs.
+      # 'x86_64-linux' and 'aarch64-linux' are for Linux platforms.
+      systems = [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
 
-      packages."aarch64-darwin".default =
+      # A helper function that produces an attribute set mapping each system
+      # to its corresponding packages.
+      forAllSystems = f: builtins.listToAttrs (map (system: {
+        name = system;
+        value = f system;
+      }) systems);
+    in
+    {
+      packages = forAllSystems (system:
         let
-          system = "aarch64-darwin";
-          # Override the nixpkgs configuration for 'aarch64-darwin'
+          # Import the package set for the current system, and allow unfree packages.
           pkgs = import nixpkgs {
             inherit system;
             config = {
-              # Set 'allowUnfree' to true to allow installation of unfree packages
+              # Set 'allowUnfree' to true to allow installation of unfree packages.
               allowUnfree = true;
             };
           };
         in
-        # 'buildEnv' is a function that allows us to create an environment which includes
-        # multiple packages. We use it here to define a custom environment named 'workspace'.
-        pkgs.buildEnv {
-          # The name of the environment, which can be anything you choose.
-          name = "workspace";
-          # 'paths' defines the list of packages that will be included in this environment.
-          # We use the 'with' keyword to bring 'pkgs' into scope, allowing us to list packages
-          # without prefixing them with 'pkgs.' each time.
-          paths = with pkgs; [
-            # Here we list the packages we want to include. We start with 'git', which is
-            # a widely-used version control system.
-            # You can add additional packages to this list as needed.
-            # Just list their names here, and Nix will handle the installation.
+        {
+          # Define the 'default' package environment for each system.
+          default = pkgs.buildEnv {
+            # The name of the environment, which can be anything you choose.
+            name = "workspace";
 
-            #handbrake
-            #nodePackages."@nrwl/cli"
-            #packer
-            #terraform
-            #vagrant
-            #vlc
-            _1password-cli
-            #ansible
-            #autotrace
-            #awscli
-            #cargo
-            curl
-            fasd
-            ffmpeg
-            #fuse
-            fzf
-            git
-            gimp
-            graphviz
-            htop
-            #inkscape
-            imagemagick
-            inetutils
-            jq
-            #kitty
-            less
-            lua
-            neovim
-            #nerdfonts
-            #nixfmt-rfc-style
-            nmap
-            #nodePackages."@angular/cli"
-            #nodejs
-            ollama
-            #go
-            pandoc
-            pgcli
-            pipx
-            podman
-            podman-compose
-            potrace
-            colima
-            docker
-            docker-compose
-            poetry
-            pv
-            #python3
-            python310
-            protobuf
-            rclone
-            ripgrep
-            rsync
-            rustc
-            screenfetch
-            stow
-            tmux
-            tree
-            wget
-            #wireshark
-            #yarn
-            zoxide
-            zsh
-            terraform
-            # Mac only
-            #hexfiend
-            #osmctools
-            #osmium-tool
-            #aria2
-            #toybox
-          ];
-        };
+            # 'paths' defines the list of packages to include in this environment.
+            # This block selects a package list depending on the platform.
+            # For Mac (aarch64-darwin), we use your detailed list and comments.
+            # For Linux, you can fill in or modify your packages as needed.
+            paths = with pkgs; (
+              if system == "aarch64-darwin" then [
+                # ----- Mac Packages -----
+                #handbrake
+                #nodePackages."@nrwl/cli"
+                #packer
+                #terraform
+                #vagrant
+                #vlc
+                _1password-cli
+                #ansible
+                #autotrace
+                #awscli
+                #cargo
+                curl
+                fasd
+                ffmpeg
+                #fuse
+                fzf
+                git
+                gimp
+                graphviz
+                htop
+                #inkscape
+                imagemagick
+                inetutils
+                jq
+                #kitty
+                less
+                lua
+                neovim
+                #nerdfonts
+                #nixfmt-rfc-style
+                nmap
+                #nodePackages."@angular/cli"
+                #nodejs
+                ollama
+                #go
+                pandoc
+                pgcli
+                pipx
+                podman
+                podman-compose
+                potrace
+                colima
+                docker
+                docker-compose
+                poetry
+                pv
+                #python3
+                python310
+                protobuf
+                rclone
+                ripgrep
+                rsync
+                rustc
+                screenfetch
+                stow
+                tmux
+                tree
+                wget
+                #wireshark
+                #yarn
+                zoxide
+                zsh
+                terraform
+                # Mac only
+                #hexfiend
+                #osmctools
+                #osmium-tool
+                #aria2
+                #toybox
+              ] else [
+                # ----- Linux Packages -----
+                fasd
+                ffmpeg
+                fzf
+                git
+                htop
+                jq
+                lua
+                neovim
+                rclone
+                rsync
+                screenfetch
+                stow
+                tmux
+                tree
+                wget
+                zsh
+              ]
+            );
+          };
+        }
+      );
     };
 }
